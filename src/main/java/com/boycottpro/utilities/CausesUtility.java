84b25855be97208cbe0e2fd4fe892e6a430424d5
schnarbiemeows <dylnksslr53@gmail.com>
2025-06-16 11:53:26 -0400

    adding CausesUtility

---
+++ src/main/java/com/boycottpro/utilities/CausesUtility.java
@@ -0,0 +1,18 @@
+package com.boycottpro.utilities;
+
+import com.boycottpro.models.Causes;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+import java.util.Map;
+
+public class CausesUtility {
+
+    private static Causes mapToCauses(Map<String, AttributeValue> item) {
+        Causes cause = new Causes();
+        cause.setCause_id(item.get("cause_id").s());
+        cause.setCategory(item.get("category").s());
+        cause.setCause_desc(item.get("cause_desc").s());
+        cause.setFollower_count(Integer.parseInt(item.getOrDefault("follower_count", AttributeValue.fromN("0")).n()));
+        return cause;
+    }
+}
