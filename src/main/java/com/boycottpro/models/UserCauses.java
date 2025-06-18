0fe322495635f40be6d47f63e77238b98ce23fd3
schnarbiemeows <dylnksslr53@gmail.com>
2025-06-18 19:57:04 -0400

    adding adding constructor to UserCauses

--- src/main/java/com/boycottpro/models/UserCauses.java
+++ src/main/java/com/boycottpro/models/UserCauses.java
@@ -8,6 +8,17 @@ public class UserCauses {
     private String user_id;
     private String cause_id;
     private String cause_desc;
+    private String timestamp;
+
+    public UserCauses() {
+    }
+
+    public UserCauses(String user_id, String cause_id, String cause_desc, String timestamp) {
+        this.user_id = user_id;
+        this.cause_id = cause_id;
+        this.cause_desc = cause_desc;
+        this.timestamp = timestamp;
+    }
 
     @DynamoDbPartitionKey
     public String getUser_id() { return user_id; }
@@ -19,4 +30,12 @@ public class UserCauses {
 
     public String getCause_desc() { return cause_desc; }
     public void setCause_desc(String cause_desc) { this.cause_desc = cause_desc; }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
 }
